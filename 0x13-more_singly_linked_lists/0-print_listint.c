54ab47fe7a2d659cbd2b22780e65198e952982ab
Don-kay <daviskrisa1@yahoo.com>
2022-11-30 09:57:06 -0800

    list of elements

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -1,26 +1,20 @@
-#include <stdio.h>
-#include <stdlib.h>
 #include "lists.h"
 
 /**
-  * print_listint - Prints all the elements of a linked list
-  * @h: The head of the linked list
-  *
-  * Return: The number of nodes of the linked list
-  */
-
+ * print_listint - prints all the elements of a list.
+ * @h: head of a list.
+ *
+ * Return: numbers of nodes.
+ */
 size_t print_listint(const listint_t *h)
 {
-	size_t node = 0;
+	size_t nnodes = 0;
 
-	if (h != NULL)
+	while (h != NULL)
 	{
-		while (h)
-		{
-			node++;
-			h = h->next;
-		}
+		printf("%d\n", h->n);
+		h = h->next;
+		nnodes++;
 	}
-	return (node);
+	return (nnodes);
 }
-
